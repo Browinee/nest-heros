33b4fe25f59e0da02f806ad9167b99c222e915de
Browinee <nihilitypeo@gmail.com>
2023-07-26 20:47:15 +0800

    feat: add user, role, permission entity

--- meeting_room_booking_system_server/src/app.module.ts
+++ meeting_room_booking_system_server/src/app.module.ts
@@ -3,20 +3,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { UserModule } from './user/user.module';
-
+import { User } from './user/entities/user.entity';
+import { Role } from './user/entities/role.entity';
+import { Permission } from './user/entities/permission.entity';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
+      host: '127.0.0.1',
       port: 3306,
       username: 'root',
       password: 'root1234',
       database: 'meeting_room_booking_system',
       synchronize: true,
       logging: true,
-      entities: [],
+      entities: [User, Role, Permission],
       poolSize: 10,
       connectorPackage: 'mysql2',
       extra: {
--- meeting_room_booking_system_server/src/user/entities/user.entity.ts
+++ meeting_room_booking_system_server/src/user/entities/user.entity.ts
@@ -1,1 +1,79 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinTable,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Role } from './role.entity';
+@Entity({
+  name: 'users',
+})
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    length: 50,
+    comment: 'username',
+  })
+  username: string;
+
+  @Column({
+    length: 50,
+    comment: 'password',
+  })
+  password: string;
+  @Column({
+    name: 'nick_name',
+    length: 50,
+    comment: '',
+  })
+  nickName: string;
+
+  @Column({
+    comment: '',
+    length: 50,
+  })
+  email: string;
+
+  @Column({
+    comment: '',
+    length: 100,
+    nullable: true,
+  })
+  headPic: string;
+
+  @Column({
+    comment: '',
+    length: 20,
+    nullable: true,
+  })
+  phoneNumber: string;
+
+  @Column({
+    comment: '',
+    default: false,
+  })
+  isFrozen: boolean;
+
+  @Column({
+    comment: '',
+    default: false,
+  })
+  isAdmin: boolean;
+
+  @CreateDateColumn()
+  createTime: Date;
+
+  @UpdateDateColumn()
+  updateTime: Date;
+
+  @ManyToMany(() => Role)
+  @JoinTable({
+    name: 'user_roles',
+  })
+  roles: Role[];
+}
