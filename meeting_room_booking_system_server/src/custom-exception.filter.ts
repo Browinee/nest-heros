b53461bb0abd0a1109040cb21a8e489cba011f35
Browinee <nihilitypeo@gmail.com>
2023-09-27 21:04:17 +0800

    feat: moidfy exception filter to get correct message

--- meeting_room_booking_system_server/src/custom-exception.filter.ts
+++ meeting_room_booking_system_server/src/custom-exception.filter.ts
@@ -12,10 +12,11 @@ export class CustomExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const response = host.switchToHttp().getResponse<Response>();
     response.statusCode = exception.getStatus();
+
     response
       .json({
         code: exception.getStatus(),
-        message: 'fail',
+        message: exception.getResponse(),
         data: exception.message,
       })
       .end();
