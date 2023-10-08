45a5c5a1809257309dac03a4df21b035efe14127
Browinee <nihilitypeo@gmail.com>
2023-10-08 19:14:29 +0800

    feat: add update password in admin

--- meeting_room_booking_system_frontend_admin/src/api/user.ts
+++ meeting_room_booking_system_frontend_admin/src/api/user.ts
@@ -1,4 +1,5 @@
 import { UserInfo } from "../pages/InfoModify";
+import { UpdatePassword } from "../pages/PasswordModify";
 import { axiosInstance } from "./base";
 
 export async function login(username: string, password: string) {
@@ -43,4 +44,15 @@ export async function updateInfo(data: UserInfo) {
 
 export async function updateUserInfoCaptcha() {
   return await axiosInstance.get("/user/update/captcha");
+}
+export async function updatePasswordCaptcha(email: string) {
+  return await axiosInstance.get("/user/update_password/captcha", {
+    params: {
+      address: email,
+    },
+  });
+}
+
+export async function updatePassword(data: UpdatePassword) {
+  return await axiosInstance.post("/user/admin/update_password", data);
 }
--- meeting_room_booking_system_frontend_admin/src/pages/PasswordModify/index.tsx
+++ meeting_room_booking_system_frontend_admin/src/pages/PasswordModify/index.tsx
@@ -1,3 +1,131 @@
+import { Button, Form, Input, message } from "antd";
+import { useForm } from "antd/es/form/Form";
+import "./index.css";
+import { useCallback, useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { getUserInfo, updatePasswordCaptcha } from "../../api";
+
+export interface UpdatePassword {
+  email: string;
+  captcha: string;
+  password: string;
+  confirmPassword: string;
+}
+
+const layout1 = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 18 },
+};
+
+const layout2 = {
+  labelCol: { span: 0 },
+  wrapperCol: { span: 24 },
+};
+
 export function PasswordModify() {
-  return <div>PasswordModify</div>;
+  const [form] = useForm();
+  const navigate = useNavigate();
+
+  const onFinish = useCallback(async (values: UpdatePassword) => {
+    if (values.password !== values.confirmPassword) {
+      return message.error("两次密码不一致");
+    }
+
+    const res = await updatePassword({
+      ...values,
+      username: form.getFieldValue("username"),
+    });
+
+    const { message: msg, data } = res.data;
+
+    if (res.status === 201 || res.status === 200) {
+      message.success("Update password successfully.");
+    } else {
+      message.error(data || "try later");
+    }
+  }, []);
+
+  const sendCaptcha = useCallback(async function () {
+    const address = form.getFieldValue("email");
+    if (!address) {
+      return message.error("email is empty");
+    }
+
+    const res = await updatePasswordCaptcha(address);
+    if (res.status === 201 || res.status === 200) {
+      message.success(res.data.data);
+    } else {
+      message.error("try later");
+    }
+  }, []);
+
+  useEffect(() => {
+    async function query() {
+      const res = await getUserInfo();
+
+      const { data } = res.data;
+
+      if (res.status === 201 || res.status === 200) {
+        form.setFieldValue("username", data.username);
+        form.setFieldValue("email", data.email);
+      }
+    }
+    query();
+  }, []);
+  return (
+    <div id="updatePassword-container">
+      <Form
+        form={form}
+        {...layout1}
+        onFinish={onFinish}
+        colon={false}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="password"
+          name="password"
+          rules={[{ required: true, message: "Please enter password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="confirmPassword"
+          name="confirmPassword"
+          rules={[{ required: true, message: "Please enter confirmPassword!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
+        <Form.Item
+          label="email"
+          name="email"
+          rules={[
+            { required: true, message: "Please enter email!" },
+            { type: "email", message: "Please enter valid email!" },
+          ]}
+        >
+          <Input disabled />
+        </Form.Item>
+
+        <div className="captcha-wrapper">
+          <Form.Item
+            label="captcha"
+            name="captcha"
+            rules={[{ required: true, message: "Please enter captcha!" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Button type="primary" onClick={sendCaptcha}>
+            Send captcha
+          </Button>
+        </div>
+
+        <Form.Item {...layout1} label=" ">
+          <Button className="btn" type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
